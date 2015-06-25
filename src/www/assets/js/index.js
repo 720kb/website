b23019d09a286a7fdb1b5480dd95e1a8518595cd
45kb <filippo.oretti@gmail.com>
2015-06-25 10:46:46 +0200

    Going on

--- src/www/assets/js/index.js
+++ src/www/assets/js/index.js
@@ -1,18 +1,28 @@
 /*global angular window*/
-(function (angular) {
+(function withAngular(angular) {
 
 'use strict';
 
 angular.module('website', [
+  'ngRoute',
   '720kb.fx'
 ])
-.controller('Ctrl', ['$scope', '$location', function ($scope, $location) {
+.config(['$locationProvider', function ($locationProvider) {
+
+  $locationProvider.html5Mode({
+    'enabled': true,
+    'requireBase': false
+  });
+}])
+.controller('Ctrl', ['$scope', '$location',
+  function Ctrl($scope, $location) {
+
+  var hash;
 
   $scope.activeLink = 'hello';
 
   $scope.setActiveLink = function setActiveLink(link) {
-
-      $scope.activeLink = link;
+    $scope.activeLink = link;
   };
 }]);
 }(angular));
