75885c1bf85b93811b3fd1137417b20c5237a672
45kb <filippo.oretti@gmail.com>
2016-02-29 14:15:45 +0100

    cookies law alert

--- src/www/assets/js/720kb.cookies.js
+++ src/www/assets/js/720kb.cookies.js
@@ -1,27 +1,27 @@
 (function () {
 
-    var cookieDiv = document.getElementById('cookies')
-    , cookieBtn = document.getElementById('720kb-cookie-button')
-    , cookieAccept = function () {
+  var cookieDiv = document.getElementById('cookies')
+  , cookieBtn = document.getElementById('720kb-cookie-button')
+  , cookieAccept = function cookieAccept() {
 
-      localStorage['720kb-cookie-accept'] = true;
-      toggleCookieAlert();
-    }
-    , toggleCookieAlert = function () {
-
-      if (!localStorage['720kb-cookie-accept']) {
-
-        cookieDiv.style.display = 'initial';
-      } else {
-
-        cookieDiv.style.display = 'none';
-      }
-    };
-
-    cookieBtn.onclick = function () {
-
-      cookieAccept();
-    };
-
+    localStorage['720kb-cookie-accept'] = true;
     toggleCookieAlert();
-  }());
+  }
+  , toggleCookieAlert = function toggleCookieAlert() {
+
+    if (!localStorage['720kb-cookie-accept']) {
+
+      cookieDiv.style.display = 'initial';
+    } else {
+
+      cookieDiv.style.display = 'none';
+    }
+  };
+
+  cookieBtn.onclick = function onCookieClick() {
+
+    cookieAccept();
+  };
+
+  toggleCookieAlert();
+}());
