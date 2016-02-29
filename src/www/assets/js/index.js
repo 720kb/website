c91bd8b7bc18b69a9db51e5432028c885e6f09bd
45kb <filippo.oretti@gmail.com>
2016-02-29 17:32:55 +0100

    smooth video and leave/back event to rewind it

--- src/www/assets/js/index.js
+++ src/www/assets/js/index.js
@@ -1,105 +1,128 @@
 /*global angular window*/
 (function withAngular(angular) {
 
-'use strict';
-var ConfigFunction = function ($locationProvider) {
+  'use strict';
 
-    $locationProvider.html5Mode({
-      'enabled': true,
-      'requireBase': false
-    }).hashPrefix('#');
-  }
-  , HomeController = function HomeController($scope, $location, $window) {
+  var ConfigFunction = function ($locationProvider) {
 
-  var that = this
-    , hash
-    , setActiveLink = function setActiveLink(link) {
-      that.activeLink = link;
+      $locationProvider.html5Mode({
+        'enabled': true,
+        'requireBase': false
+      }).hashPrefix('#');
     }
-    , mailTo = function mailTo(prefix) {
+    , HomeController = function HomeController($scope, $location, $window) {
 
-      var affix = '@'
-        , antispam = '720kb.net';
-
-        return prefix + affix + antispam;
-
-    }
-    , toggleMobileMenu = function toggleMobileMenu(event, toggle) {
-
-      event.preventDefault();
-
-      if (toggle) {
-        that.mobileMenu = !that.mobileMenu;
+    var that = this
+      , hash
+      , setActiveLink = function setActiveLink(link) {
+        that.activeLink = link;
       }
-    };
+      , mailTo = function mailTo(prefix) {
 
-  if ($location.$$hash) {
+        var affix = '@'
+          , antispam = '720kb.net';
 
-    that.activeLink = $location.$$hash;
-  } else {
+          return prefix + affix + antispam;
 
-    that.activeLink = 'hello';
-  }
-
-  that.mailTo = mailTo;
-  that.setActiveLink =  setActiveLink;
-  that.toggleMobileMenu = toggleMobileMenu;
-}
-, NavbarDirective = function navbarDirective($window) {
-  return {
-    /*'restrict': 'C',
-    'scope': true,
-    'link': function navbarDirective(scope, element, attrs) {
-
-      var theWindow = angular.element($window);
-      if (theWindow[0].scrollY > 100) {
-
-        element.addClass('scrolling');
       }
-      theWindow.bind('scroll', function onWindowScrollNavbar() {
-        if (theWindow[0].scrollY > 60) {
+      , toggleMobileMenu = function toggleMobileMenu(event, toggle) {
 
-          element.addClass('scrolling');
-        } else {
-          element.removeClass('scrolling');
+        event.preventDefault();
+
+        if (toggle) {
+          that.mobileMenu = !that.mobileMenu;
         }
-      });
-    }*/
-  };
-}
-, VideoDirective = function videoDirective($window) {
+      };
 
-  return {
-    'restrict':'A',
-    'scope': true,
-    'link': function linkingFunction(scope, element, attr) {
-      element[0].pause();
-      element[0].playbackRate = 0.65;
+    if ($location.$$hash) {
 
-      angular.element(element).bind('timeupdate', function (e) {
+      that.activeLink = $location.$$hash;
+    } else {
+
+      that.activeLink = 'hello';
+    }
+
+    that.mailTo = mailTo;
+    that.setActiveLink =  setActiveLink;
+    that.toggleMobileMenu = toggleMobileMenu;
+  }
+  , VideoDirective = function videoDirective($window) {
+
+    return {
+      'restrict':'A',
+      'scope': true,
+      'link': function linkingFunction(scope, element, attr) {
+
+        var hidden
+          , visibilityChange
+          , handleVisibilityChange = function handleVisibilityChange(videoElement) {
+
+            if ($window.document[hidden]) {
+
+              videoElement.pause();
+              videoElement.currentTime = 0;
+              videoElement.playbackRate = 0.65;
+
+            } else {
+
+              videoElement.play();
+            }
+          };
+
+        if (typeof $window.document.hidden !== 'undefined') {
+
+          hidden = 'hidden';
+          visibilityChange = 'visibilitychange';
+        } else if (typeof $window.document.mozHidden !== 'undefined') {
+
+          hidden = 'mozHidden';
+          visibilityChange = 'mozvisibilitychange';
+        } else if (typeof $window.document.msHidden !== 'undefined') {
+
+          hidden = 'msHidden';
+          visibilityChange = 'msvisibilitychange';
+        } else if (typeof $window.document.webkitHidden !== 'undefined') {
+
+          hidden = 'webkitHidden';
+          visibilityChange = 'webkitvisibilitychange';
+        }
+
+        element[0].pause();
+        element[0].playbackRate = 0.65;
+
+        $window.document.addEventListener(visibilityChange, handleVisibilityChange.bind(this, element[0]), false);
+        angular.element(element).bind('timeupdate', function onTimeUpdateVideo(e) {
+
           if (e.target.currentTime > 5.9 &&
-              e.target.currentTime < 6.2){
+              e.target.currentTime < 6.2) {
 
-              element[0].playbackRate = 0.55;
+            element[0].playbackRate = 0.55;
+
           } else if (e.target.currentTime >= 6.2 &&
             e.target.currentTime < 6.9) {
 
             element[0].playbackRate = 0.4;
+
           } else if (e.target.currentTime >= 6.9) {
 
             element[0].playbackRate = 0.25;
+
+          } else if (e.target.currentTime >= 7) {
+
+            element[0].playbackRate = 0.18;
           }
-      });
-      element[0].play();
-    }
+        });
+
+        element[0].play();
+      }
+    };
   };
-}
-angular.module('website', [
-  'ngRoute',
-  '720kb.fx'
-])
-.config(['$locationProvider', ConfigFunction])
-.controller('HomeController', ['$scope', '$location', '$window', HomeController])
-.directive('navbar', ['$window', NavbarDirective])
-.directive('videoHome',['$window', VideoDirective]);
+
+  angular.module('website', [
+    'ngRoute',
+    '720kb.fx'
+  ])
+  .config(['$locationProvider', ConfigFunction])
+  .controller('HomeController', ['$scope', '$location', '$window', HomeController])
+  .directive('videoHome',['$window', VideoDirective]);
 }(angular));
